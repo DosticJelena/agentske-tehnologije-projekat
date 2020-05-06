caf9323b77b757dc7498724e73af726b9f50ed95
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-06 17:59:39 +0200

    created AID class
    - agent id

---
+++ JarAT2020/src/agents/AID.java
@@ -0,0 +1,42 @@
+package agents;
+
+import agentcenter.AgentCenter;
+
+public class AID {
+
+	private String name;
+	private AgentType type;
+	private AgentCenter host;
+	
+	public AID() {
+		
+	}
+	
+	public AID(String name, AgentType type, AgentCenter host) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.host = host;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public AgentType getType() {
+		return type;
+	}
+	public void setType(AgentType type) {
+		this.type = type;
+	}
+	public AgentCenter getHost() {
+		return host;
+	}
+	public void setHost(AgentCenter host) {
+		this.host = host;
+	}
+	
+	
+}
