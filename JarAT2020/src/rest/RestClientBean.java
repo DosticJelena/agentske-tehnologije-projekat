c21f5558adb501bbd8a36d5d0a76f32b0eec3740
Jelena Dostić <49482144+DosticJelena@users.noreply.github.com>
2020-05-06 12:36:57 +0200

    added paths

--- JarAT2020/src/rest/RestClientBean.java
+++ JarAT2020/src/rest/RestClientBean.java
@@ -7,6 +7,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -15,38 +16,44 @@ import javax.ws.rs.core.MediaType;
 public class RestClientBean implements RestClientRemote {
 
 	@GET
+	@Path("/agents/classes")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getAgentClasses() {
 		return "getAgentClasses";
 	}
 
 	@GET
+	@Path("/agents/running")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getRunningAgents() {
 		return "getRunningAgents";
 	}
 
 	@PUT
+	@Path("/agents/running/{type}/{name}")
 	@Produces(MediaType.TEXT_PLAIN)
 	@Consumes(MediaType.TEXT_PLAIN)
-	public String startAgent(String agentClass, String agentName) {
-		return "startAgent";
+	public String startAgent(@PathParam("type") String agentClass, String agentName) {
+		return "startAgent" + " | " + agentClass + " | " + agentName;
 	}
 
 	@DELETE
+	@Path("/agents/running/{aid}")
 	@Produces(MediaType.TEXT_PLAIN)
 	@Consumes(MediaType.TEXT_PLAIN)
-	public String stopAgent(String agentAid) {
-		return "stopAgent";
+	public String stopAgent(@PathParam("aid") String agentAid) {
+		return "stopAgent" + " | " + agentAid;
 	}
 
 	@POST
+	@Path("/messages")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String sendACLMessage() {
 		return "sendACLMessage";
 	}
 
 	@GET
+	@Path("/messages")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getPerf() {
 		return "getPerf";
--- JarAT2020/src/rest/RestServerBean.java
+++ JarAT2020/src/rest/RestServerBean.java
@@ -6,6 +6,7 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -14,55 +15,67 @@ import javax.ws.rs.core.MediaType;
 public class RestServerBean implements RestServerRemote {
 
 	@POST
+	@Path("/node1")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String registerNewNode() {
 		return "registerNewNode";
 	}
 
 	@GET
+	@Path("/agents/classes1")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String newNodeAgentClasses() {
 		return "newNodeAgentClasses";
 	}
 
+	//ponovljena - spojiti
 	@POST
+	@Path("/node2")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String notifyAboutNewNode() {
 		return "notifyAboutNewNode";
 	}
 
+	//ponovljena - spojiti
 	@POST
+	@Path("/agents/classes2")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String notifyAboutNewAgentClasses() {
 		return "notifyAboutNewAgentClasses";
 	}
 
 	@POST
+	@Path("/nodes")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String allNodes() {
 		return "allNodes";
 	}
 
+	//ponovljena - spojiti
 	@POST
+	@Path("/agents/classes3")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String allAgentClasses() {
 		return "allAgentClasses";
 	}
 
 	@POST
+	@Path("/agents/running")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String allRunningAgents() {
 		return "allRunningAgents";
 	}
 
 	@DELETE
+	@Path("/node/{alias}")
 	@Produces(MediaType.TEXT_PLAIN)
 	@Consumes(MediaType.TEXT_PLAIN)
-	public String deleteDeadNode(String nodeAlias) {
-		return "deleteDeadNode";
+	public String deleteDeadNode(@PathParam("alias") String nodeAlias) {
+		return "deleteDeadNode" + " | " + nodeAlias;
 	}
 
 	@GET
+	@Path("/node")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String handshake() {
 		return "handshake";
